737e37a21289eaafcd462eab74bebe0064b09e3f
paranoidq <paranoid.qian@gmail.com>
2017-12-22 14:56:50 +0800

    【优化】将分页组件进行剥离，形成一个独立的公用组件，并修改publish页面

--- src/components/Publish.vue
+++ src/components/Publish.vue
@@ -46,46 +46,28 @@
         </div>
       </div>
 
-      <div class="row justify-content-center">
-        <nav aria-label="">
-          <ul class="pagination" id="news-pagination">
-            <li class="page-item">
-              <a class="page-link" v-if="currentPage==1">首页</a>
-              <a class="page-link" v-else href="javascript:;" @click="goPage(1)">首页</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" v-if="currentPage<=1">上一页</a>
-              <a class="page-link" v-else href="javascript:;" @click="goPage(currentPage-1)">上一页</a>
-            </li>
+      <!-- 分页组件 -->
+      <pagination
+        :currentPage="currentPage"
+        :totalPageCount="totalPageCount"
+        ref="Pagination"
+        @change="pageChange">
+      </pagination>
 
-            <li class="page-item" v-for="item in pageList">
-              <a class="page-link" v-if="currentPage == item.key || sign == item.key">{{item.key}}</a>
-              <a class="page-link" v-else href="javascript:;" @click="goPage(item.value)">{{item.key}}</a>
-            </li>
-
-            <li class="page-item">
-              <a class="page-link" v-if="currentPage>=totalPageCount">下一页</a>
-              <a class="page-link" v-else href="javascript:;" @click="goPage(currentPage+1)">下一页</a>
-            </li>
-            <li class="page-item">
-              <a class="page-link" v-if="currentPage==totalPageCount">尾页</a>
-              <a class="page-link" v-else href="javascript:;" @click="goPage(totalPageCount)">尾页</a>
-            </li>
-          </ul>
-        </nav>
-      </div>
     </div> <!-- /container -->
   </main>
 </template>
 
 <script>
-  import {getAllPublish} from '../utils/api'
   import {getPublishTotalPageCount} from '../utils/api'
   import {getPublishCurrentPage} from '../utils/api'
   import {DateFormatter} from '../utils/DateFormatter'
 
+  import Pagination from './common/Pagination';
+
 
   export default {
+    components: {Pagination},
     name: "News",
     data() {
       return {
@@ -97,73 +79,29 @@
         // 当前页
         currentPage: 1,
         // 总页数
-        totalPageCount: 4,
-        // 页面数组列表
-        pageList: []
+        totalPageCount: 1,
       };
     },
     methods: {
       formatDate: function (timestamp) {
         return DateFormatter.formatDate(new Date(timestamp), "yyyy年MM月dd日");
       },
-      goPage: function (i) {
-        this.currentPage = i;
-        this.fetchData(i);
+      pageChange: function (page) {
+        this.currentPage = page;
+        this.renderPage();
       },
-      fetchData: function (i=1) {
+      renderPage: function () {
         getPublishTotalPageCount((data) => {
-          this.pageList = [];
           this.totalPageCount = data;
-          var tmp = null;
-
-          if (this.totalPageCount > 6) {
-            if (((this.totalPageCount-1) == (this.totalPageCount - this.currentPage)) && (this.totalPageCount - this.currentPage) > 5) {
-              for (let i=1; i<7; i++) {
-                if (i < this.signIndex) {
-                  tmp = {key: i, value: i};
-                } else if (i == this.signIndex) {
-                  tmp = {key: this.sign, value: 0};
-                } else if (i == (this.signIndex + 1) ) {
-                  tmp = {key:this.totalPageCount - 1, value:this.totalPageCount - 1 };
-                } else {
-                  tmp = {key:this.totalPageCount, value:this.totalPageCount };
-                }
-                this.pageList.push(tmp)
-              }
-            } else if (((this.totalPageCount - this.currentPage) <= this.signIndex)){
-              var starNum = this.totalPageCount - 5;
-              for (let i=starNum;i<starNum+6;i++) {
-                tmp = {key:i, value:i }
-                this.pageList.push(tmp)
-              }
-            } else {
-              var starNum = this.currentPage - 1;
-              for (let i=1;i<7;i++) {
-                if (i < this.signIndex) {
-                  tmp = {key:(starNum - 1) + i, value:(starNum - 1) + i }
-                } else if (i== this.signIndex) {
-                  tmp = {key:this.sign, value:0 }
-                } else if (i == (this.signIndex + 1) ) {
-                  tmp = {key:this.totalPageCount - 1, value:this.totalPageCount - 1 }
-                } else {
-                  tmp = {key:this.totalPageCount, value:this.totalPageCount }
-                }
-                this.pageList.push(tmp)
-              }
-            }
-          } else {
-            for (var i =0; i <this.totalPageCount; i++) {
-              tmp = {key:i+1, value:i+1 }
-              this.pageList.push(tmp)
-            }
-          }
-        }).then(getPublishCurrentPage(i, (data) => {
-          this.items = data;
-        }));
+          getPublishCurrentPage(this.currentPage, (data) => {
+            this.items = data;
+            this.$refs.Pagination.renderPagination();
+          })
+        });
       }
     },
     mounted: function () {
-      this.fetchData();
+      this.renderPage();
     },
   }
 
